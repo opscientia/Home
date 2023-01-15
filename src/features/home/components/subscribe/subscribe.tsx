d7f4e9841d1aa1daa2c9061d9352ed6d7ec97767
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-01-15 18:04:33 +0530

    form done

--- src/features/home/components/subscribe/subscribe.tsx
+++ src/features/home/components/subscribe/subscribe.tsx
@@ -10,6 +10,11 @@ import {
   useTheme,
 } from "@mui/material";
 import useWindowSize from "@hooks/windowResize";
+import { useForm, SubmitHandler } from "react-hook-form";
+type Inputs = {
+  email: string;
+};
+
 function Sponcers() {
   const theme = useTheme();
   const [offSetLeft, setOffSetLeft] = React.useState(0);
@@ -18,57 +23,83 @@ function Sponcers() {
   React.useEffect(() => {
     setOffSetLeft(left.current.getBoundingClientRect().left);
   }, [windowSize]);
+
+  const [
+    isSuccessfullySubmitted,
+    setIsSuccessfullySubmitted,
+  ] = React.useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setIsSuccessfullySubmitted(true);
+
+    console.log(data)};
   return (
     <SubscribeStyled component="section" widthoffset={offSetLeft}>
-      <Grid container justifyContent="center">
-        <Grid className="email" item md={8}>
-          <Grid container justifyContent="center" direction="row-reverse">
-            <Grid item className="rectangle" md={5} sm={12}>
-              <Box
-                sx={{
-                  [theme.breakpoints.down("md")]: {
-                    img: { visibility: "hidden", width: "100%" },
-                  },
-                }}>
-                <img src="static/images/react.png" alt="react" />
-              </Box>
-            </Grid>
-            <Grid item md={7} p={3} className="sbscribe">
-              <Stack spacing={2}>
-                <Typography
-                  ref={left}
-                  variant="h1"
-                  fontFamily="'Exo 2'"
-                  fontWeight="600"
-                  className="title-line">
-                  Subscribe
-                </Typography>
-                <Typography
-                  variant="h5"
-                  fontWeight="400"
-                  fontFamily="'Epilogue'">
-                  Subscribe to receive newsletter updates on events and
-                  activities in the DeSci space.
-                </Typography>
-                <Box className="field-wrapper">
-                  <Stack direction="row" p={1.5} width={1}>
-                    <Box>
-                      <InputBase
-                        className="emil"
-                        type="email"
-                        placeholder="Email"
-                      />
-                    </Box>
-                  </Stack>
-                  <Button fullWidth className="subscribe-btn">
-                    Subscribe
-                  </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container justifyContent="center">
+          <Grid className="email" item md={8}>
+            <Grid container justifyContent="center" direction="row-reverse">
+              <Grid item className="rectangle" md={5} sm={12}>
+                <Box
+                  sx={{
+                    [theme.breakpoints.down("md")]: {
+                      img: { visibility: "hidden", width: "100%" },
+                    },
+                  }}
+                >
+                  <img src="static/images/react.png" alt="react" />
                 </Box>
-              </Stack>
+              </Grid>
+              <Grid item md={7} p={3} className="sbscribe">
+                <Stack spacing={2}>
+                  <Typography
+                    ref={left}
+                    variant="h1"
+                    fontFamily="'Exo 2'"
+                    fontWeight="600"
+                    className="title-line"
+                  >
+                    Subscribe
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    fontWeight="400"
+                    fontFamily="'Epilogue'"
+                  >
+                    Subscribe to receive newsletter updates on events and
+                    activities in the DeSci space.
+                  </Typography>
+                  <Box className="field-wrapper">
+                    <Stack direction="row" p={1.5} width={1}>
+                      <Box>
+                        <InputBase
+                          className="emil"
+                          type="email"
+                          placeholder="Email"
+                          defaultValue="test@email.com"
+                          {...register("email")}
+                        />
+                      </Box>
+                    </Stack>
+                    <Button fullWidth className="subscribe-btn" type="submit">
+                      Subscribe
+                    </Button>
+                  </Box>
+                  {formState.isSubmitted && (
+    <div className="success">Thank you for subscribing !</div>
+  )}
+                </Stack>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </form>
     </SubscribeStyled>
   );
 }
