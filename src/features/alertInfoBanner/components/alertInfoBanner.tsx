d2f8c326c78f10578b7e43a114ff1bca44b39709
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-02-03 12:20:09 +0530

    add alertBannerInfo Component file

---
+++ src/features/alertInfoBanner/components/alertInfoBanner.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+import { Link } from '@mui/material';
+
+export default function TransitionAlerts() {
+  const [open, setOpen] = React.useState(true);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Collapse in={open}>
+        <Alert
+        severity="info"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Filecoin published a case study on OpSci, Check it out ! {"  "}
+          <Link href='https://docsend.com/view/mhrxeqcc2zkcb6g4'>
+            Case Study
+          </Link>
+        </Alert>
+      </Collapse>
+      {/* <Button
+        disabled={open}
+        variant="outlined"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Re-open
+      </Button> */}
+    </Box>
+  );
+}
