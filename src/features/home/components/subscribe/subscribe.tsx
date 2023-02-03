d15bf25e0f18fbce3b31f3701ec264249fe14c09
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-02-03 17:12:56 +0530

    add snackbar

--- src/features/home/components/subscribe/subscribe.tsx
+++ src/features/home/components/subscribe/subscribe.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-// import connect from "src/lib/database";
-// import Subscriber from "src/models/Subscriber";
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import SubscribeStyled from "./overrides/subscribeStyle";
 
 import {
@@ -23,6 +24,8 @@ function Sponcers() {
   const theme = useTheme();
   const apiKey = process.env.SIB;
   const [offSetLeft, setOffSetLeft] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
+
   const left = React.useRef(null);
   const windowSize = useWindowSize();
   React.useEffect(() => {
@@ -33,10 +36,17 @@ function Sponcers() {
     React.useState(false);
 
   const { register, handleSubmit, watch, formState } = useForm<Inputs>();
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    setOpen(true);
     setIsSuccessfullySubmitted(true);
-    // const newSubscriber = new Subscriber({ email: data.email });
-    // await newSubscriber.save();
+
     console.log(data);
     console.log(apiKey)
 
@@ -56,6 +66,22 @@ function Sponcers() {
       .then((response) => console.log(response))
       .catch((err) => console.error(err));
   };
+
+  const action = (
+    <React.Fragment>
+      {/* <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button> */}
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <SubscribeStyled component="section" widthoffset={offSetLeft}>
@@ -109,7 +135,13 @@ function Sponcers() {
                     </Button>
                   </Box>
                   {formState.isSubmitted && (
-                    <div className="success">Thank you for subscribing !</div>
+                     <Snackbar
+                     open={open}
+                     autoHideDuration={5000}
+                     onClose={handleClose}
+                     message="Thank you for Subscribing!"
+                     action={action}
+                   />
                   )}
                 </Stack>
               </Grid>
